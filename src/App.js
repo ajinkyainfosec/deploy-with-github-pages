1700a4a601eefd008eea1e7297fa159d1ee20acf
Ajinkya2205 <116737906+Ajinkya2205@users.noreply.github.com>
2025-02-21 17:27:10 +0530

    change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing Github Pages (After changes) Edit <code>src/App.js</code> and
+          Testing Github Pages Edit <code>src/App.js</code> and
           save to reload.
         </p>
         <a
